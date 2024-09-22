b477e6d3395eb0008f71d44cc1329f98b798fd9b
MKTripsy <janchristiansentripolca@gmail.com>
2024-09-22 17:18:50 +0800

    Added Navbar, Routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Reservation from './components/Reservation';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar>
+    </Navbar>
     <div className="App">
-      <Reservation />
+    <Routes>
+           <Route path ='/login' element = {<Login />} >Login</Route>
+           <Route path ='/payment' element = {<Payment />} >Payment</Route>
+           <Route path ='/register' element = {<Register />} >Register</Route>
+           <Route path ='/reservation' element = {<Reservation />} >Reservation</Route>
+        </Routes>
     </div>
+    </>
+    
   );
 }
 

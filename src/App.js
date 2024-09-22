ed841981c108e5c620ac67a1923f21790433c534
MKTripsy <janchristiansentripolca@gmail.com>
2024-09-22 16:35:04 +0800

    Reservation Page, Reservation CSS, and Background Images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Reservation from './components/Reservation';
 
 function App() {
   return (
     <div className="App">
+      <Reservation />
     </div>
   );
 }

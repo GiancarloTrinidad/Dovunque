14e3acf3d41ca8552a03cc3eea8c2bcf04de37bd
MKTripsy <janchristiansentripolca@gmail.com>
2024-11-10 21:28:22 +0800

    test

---
+++ src/components/MenuCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// import './ProductCard.css';
+
+const MenuCard = ({ product, addToCart }) => {
+
+  return (
+
+    <div className="product-card">
+
+      <img src={product.image} alt={product.name} />
+
+      <h3>{product.name}</h3>
+
+      <p>₱{product.price}</p>
+
+      <button onClick={() => addToCart(product)}>Add to Cart</button>
+
+    </div>
+
+  );
+
+};
+
+export default MenuCard;

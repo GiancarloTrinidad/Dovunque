4ffd866adfb59f2b4b16fb9851c40dd6e86c86eb
Giancarlo Trinidad <trinidad.gcd@gmail.com>
2024-11-10 15:19:19 +0800

    fixed modal

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -4,11 +4,20 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-import CardPaymentModal from '../components/CardPaymentModal.js';
-import QrPaymentModal from '../components/QrPaymentModal.js'
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import PaymentModal from '../components/PaymentModal.js';
 
 function Payment() {
     const [modalShow, setCardModalShow] = useState(false);
+    const [modalTitle, setModalTitle] = useState("Default Content"); 
+    const [modalContent, setModalContent] = useState("Default Content");
+
+    const openModalWithContent = (title, content) => {
+        setModalTitle(title);
+        setModalContent(content);
+        setCardModalShow(true);
+    }
 
     return (
         <>
@@ -16,24 +25,91 @@ function Payment() {
                 <Row className='mt-5 pt-3'>   
                     <h1 className='display-4'>Choose a payment method</h1>
                 </Row>
-                <Row xs={1} md={2} className="g-4 mh-0">
+                <Row xs={1} md={2} className="g-4 mh-0">                
+                    
                     <Col>
                         {/* Mastercard */}
-                        <Card>
-                            <Card.Img 
+                        <Card className="payment-card">
+                            <Card.Img
                             variant="top" 
                             src={require("../assets/payment-mastercard.png")}
-                            onClick={() => setCardModalShow(true)}
+                            onClick={() => openModalWithContent(
+                                "Credit Card",
+                                <div>
+                                    {/* Card Number */}
+                                    <Form.Group className="mb-3">
+                                        <Form.Label className="fw-semibold">Card Number</Form.Label>
+                                        <Form.Control
+                                        required
+                                        type="text"
+                                        placeholder="1234 5678 9012 3457" />
+                                        <Form.Control.Feedback type="invalid">
+                                            Please provide a valid credit card number.
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                    
+                                    {/* Cardholder's Name */}
+                                    <Form.Group className="mb-3">
+                                        <Form.Label className="fw-semibold">Cardholder's Name</Form.Label>
+                                        <Form.Control
+                                        required
+                                        type="text"
+                                        placeholder="Cardholder's Name" />
+                                        <Form.Control.Feedback type="invalid">
+                                            Please provide a valid name.
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                    
+                                    <Row>
+                                        {/* Expiration */}
+                                        <Col>
+                                        <Form.Group className="mb-3">
+                                            <Form.Label className="fw-semibold">Expiration</Form.Label>
+                                            <Form.Control
+                                            required
+                                            type="text"
+                                            placeholder="MM/YYYY" />
+                                            <Form.Control.Feedback type="invalid">
+                                                Please provide a expiration number.
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        </Col>
+                    
+                                        {/* CVV */}
+                                        <Col>
+                                        <Form.Group className="mb-3">
+                                            <Form.Label className="fw-semibold">CVV</Form.Label>
+                                            <Form.Control
+                                            required
+                                            type="text"
+                                            placeholder="&#9679;&#9679;&#9679;" />
+                                            <Form.Control.Feedback type="invalid">
+                                                Please provide a valid CVV number.
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        </Col>
+                                    </Row>
+                                    
+                                    <div className="d-grid gap-2">
+                                        <Button variant="danger" type="submit">Pay Now</Button>
+                                    </div>
+
+                                </div>
+                            )}
                             fluid/>
                         </Card>
                     </Col>
+
                     <Col>
                         {/* GCash */}
                         <Card>
                             <Card.Img 
                             variant="top" 
                             src={require("../assets/payment-gcash.png")}
-                            // onClick={() => setQrModalShow(true)} 
+                            onClick={() => openModalWithContent(
+                                "GCash",
+                                <Image src={require('../assets/payment-gcash-qr.jpg')} fluid/>
+                            )}
                             fluid/>
                         </Card>
                     </Col>
@@ -41,15 +117,12 @@ function Payment() {
                 <Row></Row>
             </Container>
 
-            <CardPaymentModal
+            <PaymentModal
                 show={modalShow}
-                onHide={() => setCardModalShow(false)}
+                title={modalTitle}
+                content={modalContent}
+                onHide={() => setCardModalShow(false)}   
             />
-
-            {/* <QrPaymentModal
-                show={modalShow}
-                onHide={() => setQrModalShow(false)}
-            /> */}
         </>
     );
 }
